76eecc24fb88a231128b44eaf08564dac133d278
James Karpelli <140459376+JamesKarpelli@users.noreply.github.com>
2023-07-28 20:41:47 +0300

    source control system

---
+++ Source Control System API/Server/SourceControlSystem.Api/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,118 @@
+﻿namespace SourceControlSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Api.Infrastructure.Mapping;
+    using AutoMapper;
+
+    public static class AutoMapperConfig
+    {
+        private static TypeInfo ProfileTypeInfo = typeof(Profile).GetTypeInfo();
+
+        public static void RegisterMappings(params Assembly[] assemblies)
+        {
+            var allTypes = assemblies.SelectMany(a => a.GetExportedTypes().Select(t => t.GetTypeInfo()));
+
+            var profiles = allTypes
+                .Where(t => ProfileTypeInfo.IsAssignableFrom(t))
+                .Where(t => !t.IsAbstract);
+
+            Mapper.Initialize(configuration =>
+            {
+                configuration.AddProfile(StandardMappingsProfile.From(allTypes));
+                configuration.AddProfile(CustomMappingsProfile.From(allTypes));
+
+                foreach (var profile in profiles)
+                {
+                    configuration.AddProfile(profile);
+                }
+            });
+        }
+        
+        private class StandardMappingsProfile : Profile
+        {
+            private static Type MapFromType = typeof(IMapFrom<>);
+
+            private readonly IEnumerable<TypeInfo> types;
+
+            public static StandardMappingsProfile From(IEnumerable<TypeInfo> types)
+                => new StandardMappingsProfile(types);
+
+            private StandardMappingsProfile(IEnumerable<TypeInfo> types)
+            {
+                this.types = types;
+                this.LoadMappings();
+            }
+
+            private void LoadMappings()
+            {
+                var maps = this.types
+                    .SelectMany(
+                        t => t.GetInterfaces(),
+                        (t, i) => new
+                        {
+                            Type = t,
+                            Interface = i.GetTypeInfo()
+                        })
+                    .Where(
+                        map =>
+                            map.Interface.IsGenericType
+                            && map.Interface.GetGenericTypeDefinition() == MapFromType
+                            && !map.Type.IsAbstract
+                            && !map.Type.IsInterface)
+                    .Select(type => new
+                    {
+                        Source = type.Interface.GetGenericArguments()[0],
+                        Destination = type.Type.AsType()
+                    });
+
+                foreach (var map in maps)
+                {
+                    CreateMap(map.Source, map.Destination);
+                    CreateMap(map.Destination, map.Source);
+                }
+            }
+        }
+
+        private class CustomMappingsProfile : Profile
+        {
+            private static TypeInfo CustomMappingsTypeInfo = typeof(IHaveCustomMappings).GetTypeInfo();
+
+            private readonly IEnumerable<TypeInfo> types;
+
+            public static CustomMappingsProfile From(IEnumerable<TypeInfo> types)
+                => new CustomMappingsProfile(types);
+
+            private CustomMappingsProfile(IEnumerable<TypeInfo> types)
+            {
+                this.types = types;
+                this.LoadMappings();
+            }
+
+            private void LoadMappings()
+            {
+                var maps = this.types
+                    .SelectMany(
+                        t => t.GetInterfaces(),
+                        (t, i) => new
+                        {
+                            Type = t,
+                            Interface = i.GetTypeInfo()
+                        })
+                    .Where(
+                        map =>
+                            CustomMappingsTypeInfo.IsAssignableFrom(map.Type)
+                            && !map.Type.IsAbstract
+                            && !map.Type.IsInterface)
+                    .Select(type => (IHaveCustomMappings)Activator.CreateInstance(type.Type.AsType()));
+
+                foreach (var map in maps)
+                {
+                    map.CreateMappings(this);
+                }
+            }
+        }
+    }
+}
---
+++ Source Control System API/Tests/SourceControlSystem.Api.Tests/IntegrationTests/CommitsIntegrationTests.cs
@@ -0,0 +1,75 @@
+﻿namespace SourceControlSystem.Api.Tests.IntegrationTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Reflection;
+    using Autofac;
+    using Autofac.Integration.WebApi;
+    using Common.Constants;
+    using Models.Commits;
+    using MyTested.WebApi;
+    using Services.Data.Contracts;
+
+    [TestClass]
+    public class CommitsIntegrationTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            WebApiConfig.DependencyRegistrationAction = builder =>
+            {
+                builder.RegisterApiControllers(Assembly.Load(Assemblies.WebApi));
+                builder.RegisterInstance(TestObjectFactory.GetCommitsService()).As<ICommitsService>();
+            };
+
+            MyWebApi.Server().Starts<Startup>();
+        }
+
+        [TestMethod]
+        public void ByProjectShouldReturnCorrectResponse()
+        {
+            MyWebApi
+                .Server()
+                .Working()
+                .WithHttpRequestMessage(req => req
+                    .WithRequestUri("api/Commits/ByProject/1")
+                    .WithMethod(HttpMethod.Get))
+                .ShouldReturnHttpResponseMessage()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithResponseModelOfType<List<ListedCommitResponseModel>>()
+                .Passing(m => m.Count == 1);
+        }
+
+        [TestMethod]
+        public void ByProjectShouldReturnFastResponse()
+        {
+            var allResponseTimes = new List<double>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                MyWebApi
+                    .Server()
+                    .Working()
+                    .WithHttpRequestMessage(req => req
+                        .WithRequestUri("api/Commits/ByProject/1")
+                        .WithMethod(HttpMethod.Get))
+                    .ShouldReturnHttpResponseMessage()
+                    .WithResponseTime(time =>
+                    {
+                        allResponseTimes.Add(time.TotalMilliseconds);
+                    });
+            }
+
+            Assert.IsTrue(allResponseTimes.Average() < 100);
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            MyWebApi.Server().Stops();
+        }
+    }
+}
